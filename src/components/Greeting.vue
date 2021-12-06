4947d0be10adf4feb5de6636e674dabc5284aa75
ohmnapt <napats.work@gmail.com>
2021-12-07 00:05:15 +0700

    limitation props

---
+++ src/components/Greeting.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p v-if="age > 25">{{ msg }}</p>
+    <p v-else>You must be 25 year or older to view this message</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Greeting",
+  props: ["age"],
+  data() {
+    return {
+      msg: "Helloworld",
+    };
+  },
+};
+</script>
+
+<style scoped>
+p {
+  color: red;
+}
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <button type="button" @click="onClickAge">Update age Event</button>
+    <button type="button" @click="ageChangeFn(4)">Update age CB</button>
+    <p>The user is {{ age }} year old</p>
+    <p>{{ ageDoubled }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "User",
+  props: {
+    age: {
+      type: Number,
+      // required: true,
+      // default: 20,
+      validator(value) {
+        // this.onClickAge();
+        return value < 130;
+      },
+    },
+    ageChangeFn: Function,
+  },
+  emit: ["age-change"],
+  computed: {
+    ageDoubled() {
+      return this.age * 2;
+    },
+  },
+  methods: {
+    onClickAge() {
+      this.$emit("age-change", 3);
+    },
+  },
+};
+</script>
+
+<style></style>
